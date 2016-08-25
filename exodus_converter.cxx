385e10040b789f11b14b2f23084f43e77b99f71b
James Percival <j.percival@imperial.ac.uk>
2016-08-25 14:27:49 +0100

    Add support to read GMSH files.

--- exodus_converter.cxx
+++ exodus_converter.cxx
@@ -1,17 +1,19 @@
 #include "vtkExodusIIReader.h"
+#include "vtkGmshReader.h"
 #include "vtkMultiBlockDataSet.h"
 #include "vtkUnstructuredGrid.h"
 #include "vtkXMLUnstructuredGridWriter.h"
+#include "vtkXMLMultiBlockDataWriter.h"
+#include "vtkGmshWriter.h"
 #include "vtkAppendFilter.h"
 #include "vtkCellData.h"
 #include "vtkIntArray.h"
 #include "vtkIdTypeArray.h"
-#include "vtkGmshWriter.h"
 
 #include <unistd.h>
+#include <stdio.h>
 
-
-
+#include "exodus_converter.h"
 
 int main(int argc, char *argv[]) {
 
@@ -19,7 +21,12 @@ int main(int argc, char *argv[]) {
   char* output_format="gmsh";
   int opt;
 
-  while (  (opt = getopt(argc, argv, "i::o::")) != -1 ) { 
+  if (argc <3) {
+    print_usage();
+    return 0;
+  }
+
+  while (  (opt = getopt(argc, argv, "i::o::h")) != -1 ) { 
     switch ( opt ) {
     case 'i':
       if (optarg) {
@@ -31,31 +38,67 @@ int main(int argc, char *argv[]) {
 	output_format = optarg;
       }
       break;
+    case 'h':
+      print_usage();
+      break;
     case '?': 
       cerr << "Unknown option: '" << char(optopt) << "'!" << endl;
       break;
     }
   }
 
-  vtkExodusIIReader* r = vtkExodusIIReader::New();
+  vtkMultiBlockDataSet* mbdata = NULL;
+  vtkUnstructuredGrid* ugdata  = NULL;
 
-  r->SetFileName(argv[optind++]);
-  r->UpdateInformation();
-  r->GenerateGlobalNodeIdArrayOn();
-  r->GenerateGlobalElementIdArrayOn();
-  r->GenerateObjectIdCellArrayOn();
-  for (int i=0; i<r->GetNumberOfSideSetArrays(); i++) {
-    r->SetSideSetArrayStatus(r->GetSideSetArrayName(i),1);
+  if (strcmp(input_format,"exodus")==0 ) {
+    mbdata = read_exodusII(argv[optind++]);
+  }  else if (strcmp(input_format,"gmsh")==0 ) {
+    ugdata = read_gmsh(argv[optind++]);
+  }  else {
+    std::cout<< "Unrecognised input format: "<< input_format << std::endl;
+    return 1;
   }
-  r->Update();
+  
+  int flag;
+  
+  if (strcmp(output_format,"gmsh")==0) {
+    if (mbdata) {
+      ugdata=multiblock_to_unstucturedgrid(mbdata);
+    }
+    flag=write_gmsh(ugdata,argv[optind]);
+  } else if (strcmp(output_format,"vtu")==0) {
+    if (mbdata) {
+      ugdata=multiblock_to_unstucturedgrid(mbdata);
+    }
+    flag=write_vtu(ugdata,argv[optind]);
+  } else if (strcmp(output_format,"vtu")==0) {
+    if (mbdata) {
+      ugdata=multiblock_to_unstucturedgrid(mbdata);
+    }
+    flag=write_vtu(ugdata,argv[optind]);
+  } else if (strcmp(output_format,"vtm")==0) {
+    if (ugdata) {
+      //      mbdata=unstucturedgrid_to_multiblock(ugdata);
+    }
+    flag=write_vtm(mbdata,argv[optind]);
+  } else {
+    std::cout<< "Unrecognised output format: "<<output_format<<std::endl;
+    return 1;
+  }
+  
+  if (mbdata){
+    mbdata->Delete();
+  }
+  if (ugdata){
+    ugdata->Delete();
+  }
 
+  return flag;
+}
 
-  vtkMultiBlockDataSet* data;
-  data = r->GetOutput();
-
+vtkUnstructuredGrid* multiblock_to_unstucturedgrid(vtkMultiBlockDataSet* data) {
   vtkAppendFilter* appender = vtkAppendFilter::New();
   appender->SetMergePoints(1);
-
 
   vtkMultiBlockDataSet* sidesets = vtkMultiBlockDataSet::SafeDownCast(data->GetBlock(4));
   for (int i=0; i<sidesets->GetNumberOfBlocks(); i++) {
@@ -70,7 +113,7 @@ int main(int argc, char *argv[]) {
     ugrid->GetCellData()->AddArray(eids);
     appender->AddInputData(ugrid);
   }
-
+  
   vtkMultiBlockDataSet* regions = vtkMultiBlockDataSet::SafeDownCast(data->GetBlock(0));
   for (int i=0; i<regions->GetNumberOfBlocks(); i++) {
     vtkUnstructuredGrid* ugrid = vtkUnstructuredGrid::SafeDownCast(regions->GetBlock(i));
@@ -79,17 +122,85 @@ int main(int argc, char *argv[]) {
     appender->AddInputData(ugrid);
   }
 
-
   appender->Update();
-
-  vtkGmshWriter* gwriter = vtkGmshWriter::New();  
-  gwriter->SetFileName(argv[optind]);
-  gwriter->SetInputData(appender->GetOutput());
-  gwriter->SetBinaryWriteMode(0);
-  gwriter->Write();
-  
+  vtkUnstructuredGrid* output = vtkUnstructuredGrid::New();
+  output->ShallowCopy(appender->GetOutput());
   appender->Delete();
-  data->Delete();
-  return 1;
+
+  return output;
+}
+
+vtkMultiBlockDataSet* read_exodusII(char* fname){
+
+  std::cout << "Reading from file: " <<fname<<std::endl;
+
+  vtkExodusIIReader* r = vtkExodusIIReader::New();
+
+  r->SetFileName(fname);
+  r->UpdateInformation();
+  r->GenerateGlobalNodeIdArrayOn();
+  r->GenerateGlobalElementIdArrayOn();
+  r->GenerateObjectIdCellArrayOn();
+  for (int i=0; i<r->GetNumberOfSideSetArrays(); i++) {
+    r->SetSideSetArrayStatus(r->GetSideSetArrayName(i),1);
+  }
+  r->Update();
+
+  vtkMultiBlockDataSet* data;
+  data = r->GetOutput();
+
+  return data;
+ }
+
+ vtkUnstructuredGrid* read_gmsh(char* fname) {
+   std::cout << "Reading from GMSH file: " <<fname<<std::endl;
+   vtkGmshReader* reader= vtkGmshReader::New();
+   reader->SetFileName(fname);
+   reader->Update();
+   vtkUnstructuredGrid* ugrid = vtkUnstructuredGrid::New();
+   ugrid->ShallowCopy(reader->GetOutput());
+   reader->Delete();
+   return ugrid;
+ }
+
+int print_usage(){
+  std::cout << "usage: exodus_converter [-i input_format] [-o output_format] [-h] input_file output_file"<<std::endl;
+  return 0;
+}
+
+int write_gmsh(vtkUnstructuredGrid* data, char* fname){
+
+  std::cout << "Writing to file: " <<fname<<std::endl;
+
+  vtkGmshWriter* writer = vtkGmshWriter::New();  
+  writer->SetFileName(fname);
+  writer->SetInputData(data);
+  writer->Write();
+  writer->Delete();
+  return 0;
+}
+
+int write_vtu(vtkUnstructuredGrid* data, char* fname){
+
+  std::cout << "Writing to file: " <<fname<<std::endl;
+
+  vtkXMLUnstructuredGridWriter* writer = vtkXMLUnstructuredGridWriter::New();  
+  writer->SetFileName(fname);
+  writer->SetInputData(data);
+  writer->Write();
+  writer->Delete();
+  return 0;
+}
+
+int write_vtm(vtkMultiBlockDataSet* data, char* fname){
+
+  std::cout << "Writing to file: " <<fname<<std::endl;
+
+  vtkXMLMultiBlockDataWriter* writer = vtkXMLMultiBlockDataWriter::New();  
+  writer->SetFileName(fname);
+  writer->SetInputData(data);
+  writer->Write();
+  writer->Delete();
+  return 0;
 }
   
